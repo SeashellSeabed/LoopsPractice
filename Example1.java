28c4ade1b7461c1b742537e203481d02adecbdbe
Qealtz <dfsfghdfgdf@gmail.com>
2025-10-27 12:44:30 -0400

    bfdgdsffgdfgs

---
+++ Example1.java
@@ -0,0 +1,25 @@
+//SeashellSeabed
+//10/27/25
+// loops practice
+
+public class Example1{
+
+
+    public static int countFactors(int integer){
+        //return the number of factors in the number
+
+    }
+
+    public static boolean isPrime(int integer){
+        //return if an integer is prime or not
+
+    }
+
+    public static void main(String[]args){
+
+
+
+    }
+
+
+}
